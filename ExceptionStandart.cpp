9891b218cf6f4c5d142b4edd5451d244a2ced1be
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-06-01 18:16:29 +0700

    tambah blok catch

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-     cout << "Awal program" << endl; //penanda 1
+    cout << "Awal program" << endl; //penanda 1
     try
     {
         //deklarasi array data dengan panjang index 3
@@ -13,4 +13,11 @@ int main()
         //memanggil array element ke-5 yang berarti akan terjadi error
         cout << data.at(5) << endl;
     }
+    //kondisi jika terjadi error didalam try
+    catch (exception& e)
+    {
+        cout << e.what() << endl;
+    }
+    cout << "Baris program terakhir" << endl; //penanda 2
+    return 0;
 }
